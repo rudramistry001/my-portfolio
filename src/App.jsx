e43db97a3818b0a45083c5f56269917475af455f
RUDRA MISTRY <rudramistry74@yahoo.com>
2024-08-25 17:15:55 +0530

    navbar init

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden  text-neutral-300 antialiased selection: bg-cyan-300  selection: text-cyan-950">
+      <div className='flixed top-0 -z-1- h-full w-full'></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
     <Navbar/>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 
 import logo from "../assets/kevinRushLogo.png"; 
-import {Falinkedin} from "react-icons/fa";
+import {FaLinkedIn} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+import {FaSqaureXTwitter} from "react-icons/fa";
+import {FaInstagram} from "react-icons/fa";
 
 const Navbar = () => {
   return <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
 
     <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="logo" />
+    </div>
+
+    <div className="m-8 flex">
+        <FaLinkedIn />
+        <FaGithub />
+        < FaSqaureXTwitter/>
+        < FaInstagram/>
     </div>
   </nav>
 }
